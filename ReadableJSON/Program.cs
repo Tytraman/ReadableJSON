3e816edecbb96fc1be338e4e9430a4c4c4a5a186
Tytraman <tytraman@gmail.com>
2020-08-26 20:09:16 +0200

    changements initialisation des objets

--- ReadableJSON/Program.cs
+++ ReadableJSON/Program.cs
@@ -10,20 +10,22 @@ namespace ReadableJSON {
         static void Main(string[] args) {
             Console.WriteLine("Choisissez le fichier JSON à lire...");
 
-            OpenFileDialog dialog = new OpenFileDialog();
-            dialog.Title = "Sélectionner JSON";
-            dialog.Filter = "Fichiers JSON (*.json)|*.json";
-            dialog.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+            OpenFileDialog dialog = new OpenFileDialog {
+                Title = "Sélectionner JSON",
+                Filter = "Fichiers JSON (*.json)|*.json",
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+            };
             if(dialog.ShowDialog() == DialogResult.OK) {
                 string file = File.ReadAllText(dialog.FileName);
                 dynamic json = JsonConvert.DeserializeObject(file);
                 string result = JsonConvert.SerializeObject(json, Formatting.Indented);
                 Console.WriteLine("Sélectionner emplacement de sauvegarde...");
-                SaveFileDialog saver = new SaveFileDialog();
-                saver.Title = "Séctionner emplacement de sauvegarde";
-                saver.Filter = "Fichiers JSON (*.json)|*.json";
-                saver.DefaultExt = "json";
-                saver.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop);
+                SaveFileDialog saver = new SaveFileDialog {
+                    Title = "Séctionner emplacement de sauvegarde",
+                    Filter = "Fichiers JSON (*.json)|*.json",
+                    DefaultExt = "json",
+                    InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop)
+                };
                 if(saver.ShowDialog() == DialogResult.OK) {
                     File.WriteAllText(saver.FileName, result);
                     Console.WriteLine("Fichier enregistré avec succès!\nAppuyez sur une entrée pour quitter...");
